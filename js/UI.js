8f99f2cc72ae80081342ef70b8c3fe7bb25672ab
Ghamry97 <mohamed.elghamry97@gmail.com>
2017-08-23 00:28:37 +0200

    updated uijs

--- js/UI.js
+++ js/UI.js
@@ -7,6 +7,20 @@ function UI() {
     document.getElementById("player1").style.display = "none";
     document.getElementById("player2").style.display = "none";
 
+    var container1 = document.getElementsByClassName("container1");
+    container1[0].style.display = "none";
+    var bar1 = document.getElementsByClassName("bar1");
+    bar1[0].style.display = "none";
+    var bar_fill1 = document.getElementsByClassName("bar-fill1");
+    bar_fill1[0].style.display = "none";
+
+    var container2 = document.getElementsByClassName("container2");
+    container2[0].style.display = "none";
+    var bar2 = document.getElementsByClassName("bar2");
+    bar2[0].style.display = "none";
+    var bar_fill2 = document.getElementsByClassName("bar-fill2");
+    bar_fill2[0].style.display = "none";
+
     document.getElementById("UICanvas").style.display = "inline";
 
     var canvas = document.getElementById("UICanvas");
